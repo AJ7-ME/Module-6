4e8477759055f5cd27d345d358fb18e0395b4f6e
AJ7-ME <aaravkjoshi2013@gmail.com>
2026-03-31 16:08:06 +1100

    half done!!

--- Class 1/pygame screen.py
+++ Class 1/pygame screen.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.init()
-screen = pygame.display.set_mode((400, 500))
+screen = pygame.display.set_mode((1350, 680))
 done = False
 while not done:
     for event in pygame.event.get():
